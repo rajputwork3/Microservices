750ad834e2d5d0ebb682cca828f8978cd6659e56
pranayveer <pranayveer@bluekaktus.com>
2024-10-03 09:14:35 +0530

    first commit

---
+++ SchoolAPIGateway/Program.cs
@@ -0,0 +1,32 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Load configuration from appsettings.json
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Add Ocelot to the service collection
+builder.Services.AddOcelot();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+// Use Ocelot middleware
+await app.UseOcelot();
+app.Run();
